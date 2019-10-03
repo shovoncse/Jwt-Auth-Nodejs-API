d2739f80c7579c43a862d762c0ec6671b4d0c0f3
Shovon Das <shovondas26@gmail.com>
2019-10-03 18:23:52 +0600

    User Registration

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+// Import Routes
+const authRoute = require('./routes/auth');
 
 dotenv.config();
 
@@ -10,9 +12,8 @@ mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, ()=> {
   console.log('Connected to Database')
 });
 
-// Import Routes
-const authRoute = require('./routes/auth');
-
+// Body parser Middleware
+app.use(express.json());
 // Route Middleware
 app.use('/api/user', authRoute);
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,19 @@
 const router = require('express').Router();
+const User = require('../model/User');
 
-router.post('/register', (req, res) => {
-  res.send('Register');
-});
+router.post('/register', async (req, res) => {
+  const user = new User({
+    name:req.body.name,
+    email:req.body.email,
+    password:req.body.password,
+  });
 
-router.post('/login', (req, res) => {
-  res.send('login');
+  try {
+    const savedUser = await user.save();
+    res.send(savedUser);
+  } catch (error) {
+    res.status('400').send(error);
+  }
 });
 
 module.exports = router;
