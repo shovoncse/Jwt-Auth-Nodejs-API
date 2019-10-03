b56b63012de853490d8b2040ffd1428ff72bb270
Shovon Das <shovondas26@gmail.com>
2019-10-03 16:01:48 +0600

    MongoDB Database Connection

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+// Coonect Database
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, ()=> {
+  console.log('Connected to Database')
+});
 
 // Import Routes
 const authRoute = require('./routes/auth');
@@ -7,5 +16,4 @@ const authRoute = require('./routes/auth');
 // Route Middleware
 app.use('/api/user', authRoute);
 
-
-app.listen(5000, () => console.log('Server up and running'))
+app.listen(5000, () => console.log('Server up and running'));
